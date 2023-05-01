717f868f13dfd23c30299caf23199b2e80f1466d
peterjxl <peterjxl@qq.com>
2023-05-01 22:31:41 +0800

    Spring中关于DI的代码

--- src/main/java/com/peterjxl/service/impl/AccountServiceImpl2.java
+++ src/main/java/com/peterjxl/service/impl/AccountServiceImpl2.java
@@ -8,16 +8,34 @@ import java.util.Date;
 /**
  * 账户的业务层实现类
  */
-public class AccountServiceImpl implements IAccountService {
+public class AccountServiceImpl2 implements IAccountService {
 
 
     private String name;
     private Integer age;
     private Date birthday;
 
-    public AccountServiceImpl(String name, Integer age, Date birthday) {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
         this.birthday = birthday;
     }
 
