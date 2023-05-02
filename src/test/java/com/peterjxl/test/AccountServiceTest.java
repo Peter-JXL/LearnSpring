c57f77ae8c4c40e9d2bbc6c24f9a82d0c7c7e582
peterjxl <peterjxl@qq.com>
2023-05-02 16:24:52 +0800

    Spring整合Junit

--- src/test/java/com/peterjxl/test/AccountServiceTest.java
+++ src/test/java/com/peterjxl/test/AccountServiceTest.java
@@ -1,20 +1,32 @@
 package com.peterjxl.test;
 
-import com.peterjxl.config.JdbcConfig;
 import com.peterjxl.config.SpringConfiguration;
 import com.peterjxl.domain.Account;
 import com.peterjxl.service.IAccountService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import java.util.List;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfiguration.class)
 public class AccountServiceTest {
+
+    @Autowired
+    private ApplicationContext ac ;
+
+    @Autowired
+    private IAccountService as;
 
     @Test
     public void testFindAll() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
+        ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+        ac.getBean("accountService", IAccountService.class);
         List<Account> allAccount = as.findAllAccount();
         for (Account account : allAccount) {
             System.out.println(account);
@@ -23,16 +35,12 @@ public class AccountServiceTest {
 
     @Test
     public void testFindOne() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         Account account = as.findAccountById(1);
         System.out.println(account);
     }
 
     @Test
     public void testSave() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         Account account = new Account();
         account.setName("test");
         account.setMoney(12345f);
@@ -41,8 +49,6 @@ public class AccountServiceTest {
 
     @Test
     public void testUpdate() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         Account account = as.findAccountById(16);
         account.setMoney(23456f);
         as.updateAccount(account);
@@ -50,8 +56,6 @@ public class AccountServiceTest {
 
     @Test
     public void testDelete() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         as.deleteAccount(16);
     }
 }
