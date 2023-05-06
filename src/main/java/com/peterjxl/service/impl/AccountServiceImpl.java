e065ca89bfcf62200160dad518f9f8fcf3715105
peterjxl <peterjxl@qq.com>
2023-05-06 08:08:51 +0800

    编程式事务控制

--- src/main/java/com/peterjxl/service/impl/AccountServiceImpl.java
+++ src/main/java/com/peterjxl/service/impl/AccountServiceImpl.java
@@ -3,39 +3,54 @@ package com.peterjxl.service.impl;
 import com.peterjxl.dao.IAccountDao;
 import com.peterjxl.domain.Account;
 import com.peterjxl.service.IAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
-
-@Service("accountService")
-@Transactional // 事务注解
 public class AccountServiceImpl implements IAccountService {
 
-    @Autowired
     private IAccountDao accountDao;
 
+    private TransactionTemplate transactionTemplate;
+
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
     @Override
     public Account findAccountById(Integer accountId) {
-        return accountDao.findAccountById(accountId);
+        return transactionTemplate.execute(new TransactionCallback<Account>() {
+            @Override
+            public Account doInTransaction(TransactionStatus status) {
+                return accountDao.findAccountById(accountId);
+            }
+        });
     }
 
     @Override
     public void transfer(String sourceName, String targetName, Float money) {
+        transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                // 2.1 根据名称查询转出账户
+                Account source = accountDao.findAccountByName(sourceName);
+                // 2.2 根据名称查询转入账户
+                Account target = accountDao.findAccountByName(targetName);
+                // 2.3 转出账户减钱
+                source.setMoney(source.getMoney() - money);
+                // 2.4 转入账户加钱
+                target.setMoney(target.getMoney() + money);
+                // 2.5 更新转出账户
+                accountDao.updateAccount(source);
 
-        // 2.1 根据名称查询转出账户
-        Account source = accountDao.findAccountByName(sourceName);
-        // 2.2 根据名称查询转入账户
-        Account target = accountDao.findAccountByName(targetName);
-        // 2.3 转出账户减钱
-        source.setMoney(source.getMoney() - money);
-        // 2.4 转入账户加钱
-        target.setMoney(target.getMoney() + money);
-        // 2.5 更新转出账户
-        accountDao.updateAccount(source);
-
-//        int i = 1/0;
-        // 2.6 更新转入账户
-        accountDao.updateAccount(target);
+                //int i = 1/0;
+                // 2.6 更新转入账户
+                accountDao.updateAccount(target);
+                return null;
+            }
+        });
     }
 }
--- src/test/java/com/peterjxl/test/AccountServiceTest.java
+++ src/test/java/com/peterjxl/test/AccountServiceTest.java
@@ -1,7 +1,6 @@
 package com.peterjxl.test;
 
 
-import com.peterjxl.config.SpringConfiguration;
 import com.peterjxl.service.IAccountService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = SpringConfiguration.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
 public class AccountServiceTest {
 
     @Autowired
